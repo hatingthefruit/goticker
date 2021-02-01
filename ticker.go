0eff72414e0b1d7e37c0bcb4c60d905e5d7c6dcc
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2021-01-31 18:44:06 -0600

    First commit

---
+++ ticker.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fmpSite := "https://financialmodelingprep.com/api/v3"
+	apiKey := "?apikey=3633c381ffbe8e07e7fe8d8e3196f386 "
+	response, err := http.Get(fmpSite + "/quote-short/GME" + apiKey)
+	fmt.Println(fmpSite + "/quote-short/GME" + apiKey)
+	defer response.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+
+	}
+	type Message struct {
+		Symbol string
+		Price  float64
+	}
+
+	var m []Message
+	array, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(array))
+	json.Unmarshal(array, &m)
+	fmt.Printf("%s: %f\n", m[0].Symbol, m[0].Price)
+}
